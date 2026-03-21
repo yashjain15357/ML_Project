adcfc292ed26f15f48fda8d9c10101d5b3c85b82
yash jain <161117850+yashjain15357@users.noreply.github.com>
2026-03-21 18:27:47 +0530

    setup

---
+++ setup.py
@@ -0,0 +1,29 @@
+# it help to make our ML project as the package and also we able to uplode on the pypi 
+from setuptools import find_packages , setup
+from typing import List
+
+
+HYPEN_E_DOT = '-e .'
+def get_requirments(file_path:str)->List[str]:
+    """
+    this is function will return the list of requirments
+        """
+    requirments=[]
+    with open(file_path) as file_obj:
+        requirments = file_obj.readlines()
+        [req.replace ("\n" , " ")for req in requirments]
+
+        if HYPEN_E_DOT in requirments:
+            requirments.remove(HYPEN_E_DOT)
+
+    return requirments
+
+setup(
+    name="ML_Project",
+    version='0.0.1',
+    author="Yash jain",
+    author_email="jainy9089@gmail.com",
+    packages= find_packages(),
+    install_requires=get_requirments('requirments.txt')
+
+)
